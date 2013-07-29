fb37537b18fa1fcdc49fcb2bba5d737d24893a38
Jason Frame <jason@onehackoranother.com>
2013-07-29 18:49:51 +0100

    tests

---
+++ test/all.js
@@ -0,0 +1,53 @@
+var test = require('tape');
+var MinHeap = require('../').MinHeap;
+
+function t(str, fn) {
+  test(str, function(assert) {
+    fn(assert);
+    assert.end();
+  })
+}
+
+t('initial .size', function(assert) {
+  var heap = new MinHeap;
+  assert.ok(heap.size === 0);
+});
+
+t('size after 100 inserts', function(assert) {
+  var heap = new MinHeap;
+  for (var i = 0; i < 100; ++i) {
+    heap.insert(Math.random());
+  }
+  assert.ok(heap.size === 100);
+});
+
+t('fuzz test', function(assert) {
+
+  var heap = new MinHeap;
+  var check = [];
+
+  for (var i = 0; i < 50; ++i) {
+    var v = Math.random();
+    heap.insert(v);
+    check.push(v);
+  }
+
+  check.sort();
+  for (var i = 0; i < 20; ++i) {
+    assert.ok(heap.remove() === check.shift());
+  }
+
+  for (var i = 0; i < 50; ++i) {
+    var v = Math.random();
+    heap.insert(v);
+    check.push(v);
+  }
+
+  check.sort();
+  while (heap.size) {
+    assert.ok(heap.remove() === check.shift());
+  }
+
+  assert.ok(check.length === 0);
+
+});
